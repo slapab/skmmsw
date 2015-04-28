1350751c44cec4ae185f21ada0e9620745dba4a2
Tomek <tstrzeba@student.agh.edu.pl>
2015-04-28 16:23:21 +0200

    Problem repair...

--- MDK-ARM-Tomek-PC/STM32F429I_DISCO/guiconf.d
+++
@@ -1,8 +0,0 @@
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\GUIConf.c
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\GUI.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\GUI_ConfDefaults.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\GUIConf.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\GUI_Type.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\LCD.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\Global.h
-.\stm32f429i_disco\guiconf.o: ..\Middlewares\STemWin\Config\inc\GUI_Version.h
